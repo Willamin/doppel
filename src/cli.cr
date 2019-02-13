0fc6b985a2123dd6e4a47e1b7a63ca2c93e645f0
Will Lewis <will@wflewis.com>
2019-02-12 22:05:46 -0500

    Add skeleton structure

--- src/cli.cr
+++ src/cli.cr
@@ -6,6 +6,13 @@ parser = OptionParser.new do |parser|
 
   parser.on("-v", "--version", "display the version") { puts Doppel::VERSION; exit 0 }
   parser.on("-h", "--help", "show this help") { puts parser; exit 0 }
+
+  parser.separator
+
+  parser.on("-i", "--intercept", "Enter intercept mode") { Doppel.intercept; exit 0 }
+  parser.on("-p", "--playback", "Enter playback mode") { Doppel.playback; exit 0 }
 end
 
 parser.parse!
+
+puts parser; exit 0
--- src/doppel.cr
+++ src/doppel.cr
@@ -1,3 +1,11 @@
 module Doppel
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+
+  def self.intercept
+    puts "intercepting"
+  end
+
+  def self.playback
+    puts "playing back"
+  end
 end
