87c577d2947173f34f08dfce64821dad74498da3
Will Lewis <will@wflewis.com>
2019-02-14 21:36:36 -0500

    Add capability to forward requests

--- src/cli.cr
+++ src/cli.cr
@@ -1,18 +1,41 @@
 require "option_parser"
 require "./doppel"
 
-parser = OptionParser.new do |parser|
-  parser.banner = "usage: doppel"
+todo = :nothing
+forwarding_host? : String? = nil
 
-  parser.on("-v", "--version", "display the version") { puts Doppel::VERSION; exit 0 }
-  parser.on("-h", "--help", "show this help") { puts parser; exit 0 }
+parser = OptionParser.new do |parser|
+  parser.banner = "usage: doppel "
+
+  parser.on("-v", "--version", "display the version") { todo = :version }
+  parser.on("-h", "--help", "show this help") { todo = :help }
 
   parser.separator
 
-  parser.on("-i", "--intercept", "Enter intercept mode") { Doppel.intercept; exit 0 }
-  parser.on("-p", "--playback", "Enter playback mode") { Doppel.playback; exit 0 }
+  parser.on("-i", "--intercept", "Enter intercept mode") { todo = :intercept }
+  parser.on("-p", "--playback", "Enter playback mode") { todo = :playback }
+
+  parser.separator("options")
+
+  parser.on("-H HOST", "--host HOST", "set the forwarding host") { |host| forwarding_host? = host }
 end
 
 parser.parse!
 
-puts parser; exit 0
+case todo
+when :version
+  puts Doppel::VERSION
+when :intercept
+  unless forwarding_host?
+    puts("forwarding host must be set")
+    exit(1)
+  end
+  Doppel::Interceptor.new(forwarding_host?.as(String))
+when :playback
+  Doppel.playback
+when :help
+  puts(parser)
+else
+  puts(parser)
+  exit 1
+end
--- src/doppel.cr
+++ src/doppel.cr
@@ -1,11 +1,52 @@
+require "http"
+
 module Doppel
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 
-  def self.intercept
-    puts "intercepting"
-  end
-
   def self.playback
     puts "playing back"
+  end
+
+  class Route
+    @path : String
+    @response : HTTP::Server::Response
+
+    def initialize(@path, @response); end
+  end
+
+  class Server
+    @server : HTTP::Server
+
+    def initialize(&block : HTTP::Server::Context -> )
+      @server = HTTP::Server.new(block)
+    end
+
+    def listen
+      address = @server.bind_tcp(8080)
+      yield address
+      @server.listen
+    end
+  end
+end
+
+class Doppel::Interceptor
+  def initialize(host)
+    client = HTTP::Client.new(host)
+    server = Server.new do |context|
+      request = context.request
+      request.headers["Host"] = host
+      client.exec(request) do |response|
+        response.headers.each do |key, values|
+          context.response.headers[key] = values
+        end
+        context.response.status_code = response.status_code
+        body = response.body_io.gets_to_end
+        context.response.puts(body)
+      end
+    end
+
+    server.listen do |address|
+      puts("forwarding http://#{address} to #{host} and intercepting")
+    end
   end
 end
