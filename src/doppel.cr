071ee3e79018a5c916d2fd4113cc57eef5d8dc39
Will Lewis <will@wflewis.com>
2019-02-14 22:31:00 -0500

    Remove clever but unhelpful convenience method

--- src/doppel.cr
+++ src/doppel.cr
@@ -4,22 +4,14 @@ require "./doppel/*"
 module Doppel
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 
-  class ::HTTP::Server
-    def bind_and_listen(port)
-      address = self.bind_tcp(port)
-      yield address
-      self.listen
-    end
-  end
-
   def self.run(host : String)
     server = HTTP::Server.new([
       Doppel::Interceptor.new,
       Doppel::Forwarder.new(host),
     ])
 
-    server.bind_and_listen(8080) do |address|
-      puts("#{address} --> #{host}\n")
-    end
+    address = server.bind_tcp(8080)
+    puts("#{address} --> #{host}\n")
+    server.listen
   end
 end
